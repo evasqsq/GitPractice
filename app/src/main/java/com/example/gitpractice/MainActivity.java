c6ce2b49f1b027516cffca00bc5fb828c04b36d8
Hleb Kononov <hleb.kononov@student.tuke.sk>
2024-01-06 01:50:12 +0100

    experimental commit

--- app/src/main/java/com/example/gitpractice/MainActivity.java
+++ app/src/main/java/com/example/gitpractice/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Second commit containtment");
+        System.out.println("Local Changes");
+        System.out.println("Experimental");
     }
 }
